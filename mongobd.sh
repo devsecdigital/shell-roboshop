9ebba2bb5fd13296dd80e0fff8a2b57e523e52e7
shaik hameed <shaikhameedbasha085@gmail.com>
2026-01-28 12:28:02 +0530

    hello world created

--- mongobd.sh
+++ mongobd.sh
@@ -6,31 +6,31 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo 14-logs.sh | cut -d "." -f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
-echo "script started executing at: $(date)" | tee -a $LOG_FILE
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
 # check the user has root priveleges or not
 if [ $USERID -ne 0 ]
 then
-    echo -e "$R ERROR:: please run this script with root access $N" | tee -a $LOG_FILE
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
     exit 1 #give other than 0 upto 127
 else
-    echo "you are  running with root access" | tee -a $LOG_FILE
+    echo "You are running with root access" | tee -a $LOG_FILE
 fi
 
-#validate function takes input as exit status,what command they tried to install
+# validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
-      if [ $1 -eq 0 ]
+    if [ $1 -eq 0 ]
     then
-        echo -e "$2 is .... $G SUCCESS $N" | tee -a $LOG_FILE
-    else 
-        echo -e "$2 is .... $R FAILURE $N" | tee -a $LOG_FILE
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
-    fi  
-} 
+    fi
+}
 
 cp mongo.repo /etc/yum.repos.d/mongodb.repo
 VALIDATE $? "Copying MongoDB repo"
