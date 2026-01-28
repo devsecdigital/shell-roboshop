805a8bc268002336eadcb493900ec4133a5389eb
shaik hameed <shaikhameedbasha085@gmail.com>
2026-01-28 11:53:23 +0530

    hello world created

--- mongobd.sh
+++ mongobd.sh
@@ -32,7 +32,7 @@ VALIDATE(){
     fi  
 } 
 
-cp mongodb.repo vim /etc/yum.repos.d/mongo.repo
+cp mongo.repo vim /etc/yum.repos.d/mongodb.repo
 VALIDATE $? "copying MongoDB repo"
 
 dnf install mongodb-org -y &>>$LOG_FILE
