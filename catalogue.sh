4f8a0f54d584124624d4483a2e86d233809a7d7c
shaik hameed <shaikhameedbasha085@gmail.com>
2026-01-28 16:50:40 +0530

    hello world created

--- catalogue.sh
+++ catalogue.sh
@@ -33,8 +33,8 @@ VALIDATE(){
     fi
 }
 
-dnf module disable nodejs -y &>>$LOG_FILE # will get more logs
-VALIDATE $? "Disabling  default nodejs"
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disabling default nodejs"
 
 dnf module enable nodejs:20 -y &>>$LOG_FILE
 VALIDATE $? "Enabling nodejs:20"
@@ -48,34 +48,41 @@ then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
     VALIDATE $? "Creating roboshop system user"
 else
-    echo -e "System user roboshop already created.... $Y SKPPING $N"
+    echo -e "System user roboshop already created ... $Y SKIPPING $N"
 fi
 
 mkdir -p /app 
 VALIDATE $? "Creating app directory"
 
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
-VALIDATE $? "Downloading catalogue code"
+VALIDATE $? "Downloading Catalogue"
 
 rm -rf /app/*
 cd /app 
 unzip /tmp/catalogue.zip &>>$LOG_FILE
-VALIDATE $? "Unzipping catalogue"
+VALIDATE $? "unzipping catalogue"
 
 npm install &>>$LOG_FILE
-VALIDATE $? "Installing dependencies"
+VALIDATE $? "Installing Dependencies"
 
-cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service &>>$LOG_FILE
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
 VALIDATE $? "Copying catalogue service"
 
 systemctl daemon-reload &>>$LOG_FILE
-systemctl enable catalogue &>>$LOG_FILE
+systemctl enable catalogue  &>>$LOG_FILE
 systemctl start catalogue
-VALIDATE $? "Starting catalogue"
+VALIDATE $? "Starting Catalogue"
 
-cp $SCRIPT_DIR/mongo.repo  /etc/yum.repos.d/mongo.repo 
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo 
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB Client"
 
-mongosh --host mongodb.devsec.digital </app/db/master-data.js &>>$LOG_FILE
-VALIDATE $? "loading data in MongoDB"
+STATUS=$(mongosh --host mongodb.daws84s.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+#if [ $STATUS -lt 0 ]
+#then
+    #mongosh --host mongodb.daws84s.site </app/db/master-data.js &>>$LOG_FILE
+    VALIDATE $? "Loading data into MongoDB"
+#else
+    #echo -e "Data is already loaded ... $Y SKIPPING $N"
+#fi
+
