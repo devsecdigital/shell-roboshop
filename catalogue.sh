f47b9824a2f67bdfb6e8da249846b7999415ba15
shaik hameed <shaikhameedbasha085@gmail.com>
2026-01-29 10:09:48 +0530

    hello world created

--- catalogue.sh
+++ catalogue.sh
@@ -82,11 +82,11 @@ dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB Client"
 
 STATUS=$(mongosh --host mongodb.devsec.digital --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
-#if [ $STATUS -lt 0 ]
-#then
-    #mongosh --host mongodb.daws84s.site </app/db/master-data.js &>>$LOG_FILE
+if [ $STATUS -lt 0 ]
+then
+    mongosh --host mongodb.devsec.digital </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Loading data into MongoDB"
-#else
-    #echo -e "Data is already loaded ... $Y SKIPPING $N"
-#fi
+else
+    echo -e "Data is already loaded ... $Y SKIPPING $N"
+fi
 
