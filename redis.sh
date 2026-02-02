82a1c9d7985e2262ad51e0365b0c602d98bf8666
shaik hameed <shaikhameedbasha085@gmail.com>
2026-02-02 10:12:15 +0530

    shell roboshop

--- redis.sh
+++ redis.sh
@@ -52,6 +52,6 @@ systemctl start redis  &>>$LOG_FILE
 VALIDATE $? "Started Redis"
 
 END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME-$START_TIME ))
+TOTAL_TIME=$(( "$END_TIME - $START_TIME" ))
 
 echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
